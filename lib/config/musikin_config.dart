b4caa960fe8459ca16581633bcde9977b854c3ac
arvirmdn <arviistecu@gmail.com>
2026-09-22 01:34:15 +0700

    Add files via upload

--- lib/config/musikin_config.dart
+++ lib/config/musikin_config.dart
@@ -2,5 +2,5 @@
 // Kalau nanti URL Railway-nya ganti, tinggal edit baris di bawah ini.
 class MusikinConfig {
   static const String baseUrl =
-      'https://backend-musik-apk-cntlnrxs-production.up.railway.app';
+      'https://web-production-26872.up.railway.app';
 }
